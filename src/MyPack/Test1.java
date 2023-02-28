d9d6a68a674839467294f75b4b5fcbdc36b7cece
Hema <Hema.gururani@wsa.com>
2023-02-28 14:34:41 +0800

    my first commit...

---
+++ src/MyPack/Test1.java
@@ -0,0 +1,24 @@
+package MyPack;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("hema");
+		
+		int i=1;
+		int b=6;
+	
+		i=i+b;
+		b=i-b;
+		i=i-b;
+		
+		
+		
+		System.out.println("The value of i is " + i);
+		System.out.println("The value of b is " + b);
+		
+		
+	}
+}
