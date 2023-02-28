45cb8a108a67dd0e030a21a670b8bc79c55f73b9
Hema <Hema.gururani@wsa.com>
2023-02-28 16:38:34 +0800

    committed

---
+++ src/MyPack/Test2.java
@@ -0,0 +1,20 @@
+package MyPack;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+
+
+				
+				int a =3;
+				int b=4;
+				
+				System.out.println(a+b);
+			}
+
+		}
+
